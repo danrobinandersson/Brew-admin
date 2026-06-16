35c98f42c837b1a1834d46023cacdaa37c7b76cb
Robin Andersson <danrobin.andersson@outlook.com>
2026-06-16 08:14:23 +0200

    Added login authentication

---
+++ app/Http/Controllers/LoginController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class LoginController extends Controller
+{
+    public function create()
+    {
+        return view('auth.login');
+    }
+
+    public function store(Request $request)
+    {
+        $credentials = $request->validate([
+            'email' => 'required|email',
+            'password' => 'required|string',
+        ]);
+
+        if (Auth::attempt($credentials)) {
+            $request->session()->regenerate();
+            return redirect()->intended('products');
+        }
+
+        return back()->withErrors([
+            'email' => 'Invalid email or password.',
+        ])->onlyInput('email');
+    }
+
+    public function destroy(Request $request)
+    {
+        Auth::logout();
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+
+        return redirect('/login');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,29 +1,39 @@
 <?php
 
 use App\Http\Controllers\ProductController;
+use App\Http\Controllers\LoginController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return redirect()->route('products.index');
 });
 
-// Show all products
-Route::get('/products', [ProductController::class, 'index'])->name('products.index');
+// Login routes (no auth required to access these)
+Route::get('/login', [LoginController::class, 'create'])->name('login');
+Route::post('/login', [LoginController::class, 'store'])->name('login.store');
+Route::delete('/logout', [LoginController::class, 'destroy'])->name('logout');
 
-// Show form to create a new product
-Route::get('/products/create', [ProductController::class, 'create'])->name('products.create');
+// Everything below requires login
+Route::middleware('auth')->group(function () {
 
-// Save a new product
-Route::post('/products', [ProductController::class, 'store'])->name('products.store');
+    // Show all products
+    Route::get('/products', [ProductController::class, 'index'])->name('products.index');
 
-// Show a single product
-Route::get('/products/{product}', [ProductController::class, 'show'])->name('products.show');
+    // Show form to create a new product
+    Route::get('/products/create', [ProductController::class, 'create'])->name('products.create');
 
-// Show form to edit a product
-Route::get('/products/{product}/edit', [ProductController::class, 'edit'])->name('products.edit');
+    // Save a new product
+    Route::post('/products', [ProductController::class, 'store'])->name('products.store');
 
-// Save edited product
-Route::put('/products/{product}', [ProductController::class, 'update'])->name('products.update');
+    // Show a single product
+    Route::get('/products/{product}', [ProductController::class, 'show'])->name('products.show');
 
-// Delete a product
-Route::delete('/products/{product}', [ProductController::class, 'destroy'])->name('products.destroy');
+    // Show form to edit a product
+    Route::get('/products/{product}/edit', [ProductController::class, 'edit'])->name('products.edit');
+
+    // Save edited product
+    Route::put('/products/{product}', [ProductController::class, 'update'])->name('products.update');
+
+    // Delete a product
+    Route::delete('/products/{product}', [ProductController::class, 'destroy'])->name('products.destroy');
+});
